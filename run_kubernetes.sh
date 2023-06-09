8a8591810a089994f6ff3325935c991f76b178de
root <root@ip-172-31-89-147.ec2.internal>
2023-06-09 09:02:40 +0000

    update

--- run_kubernetes.sh
+++ run_kubernetes.sh
@@ -5,14 +5,14 @@
 # Step 1:
 # This is your Docker ID/path
 # dockerpath=<>
-
+dockerpath=trangocquy123/quytn7
 # Step 2
 # Run the Docker Hub container with kubernetes
-
+kubectl run quytn7 --image=$dockerpath --port=80 --labels app=quytn7
 
 # Step 3:
 # List kubernetes pods
-
+kubectl get pods
 # Step 4:
 # Forward the container port to a host
-
+kubectl port-forward quytn7 8000:5001
--- upload_docker.sh
+++ upload_docker.sh
@@ -5,11 +5,13 @@
 
 # Step 1:
 # Create dockerpath
-# dockerpath=<your docker ID/path>
-
+dockerpath=trangocquy123/quytn7
 # Step 2:  
 # Authenticate & tag
+docker tag quytn7 $dockerpath
+
 echo "Docker ID and Image: $dockerpath"
 
 # Step 3:
 # Push image to a docker repository
+docker push $dockerpath
